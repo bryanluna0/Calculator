b0e6b48997b48f9e4cd4930d140c1fc254e29d8a
bryanluna0 <159389778+bryanluna0@users.noreply.github.com>
2024-09-06 17:57:24 -0300

    Implement numerical button functionality

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 /* Main javascript file */
+
+function append(number) {
+    if (solution == 0) {
+        console.log("was 0");
+        solution = number;
+        document.getElementById("solution").innerHTML = solution;
+        return;
+    }
+    
+    console.log("changed");
+    solution = (solution * 10) + number;
+    document.getElementById("solution").innerHTML = solution;    
+}
+
+let solution = document.querySelector("#solution");
+solution = 0;
