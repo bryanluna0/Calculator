0ccdd358d385279fccb26149ea515885161a3091
bryanluna0 <159389778+bryanluna0@users.noreply.github.com>
2024-09-06 18:08:05 -0300

    Attempt add clear, does not work

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 /* Main javascript file */
 
 function append(number) {
+    console.log("num");
     if (solution == 0) {
-        console.log("was 0");
         solution = number;
         document.getElementById("solution").innerHTML = solution;
         return;
     }
     
-    console.log("changed");
     solution = (solution * 10) + number;
     document.getElementById("solution").innerHTML = solution;    
+}
+
+function clear() {
+    console.log("clear");
+    solution = 0;
+    document.getElementById("solution").innerHTML = solution;
 }
 
 let solution = document.querySelector("#solution");
