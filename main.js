b2f4c7cfad8de42a9c3d5994fea128d77508636c
bryanluna0 <159389778+bryanluna0@users.noreply.github.com>
2024-09-06 23:37:25 -0300

    Implement all functions

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ function append(number) {
 
 function operation(op) {
     operator = op;
+    operandA = screen;
+    screen = 0;
     document.getElementById("screen").innerHTML = 0;
 }
 
@@ -23,6 +25,32 @@ function empty() {
     document.getElementById("screen").innerHTML = screen;
 }
 
+function result() {
+    console.log("result");
+    if (operator == -1) {
+        return;
+    }
+
+    operandB = screen;
+    if (operator == 0) {
+        screen = operandA * operandB;
+    }
+    else if (operator == 1) {
+        screen = operandA + operandB;
+    }
+    else if (operator == 2) {
+        screen = operandA - operandB;
+    }
+    else if (operator == 3) {
+        screen = Math.round( (operandA / operandB) * 100) / 100;
+    }
+
+    document.getElementById("screen").innerHTML = screen; 
+}
+
 let operator = -1;
 let screen = document.querySelector("#screen");
 screen = 0;
+
+let operandA = 0;
+let operandB = 0;
